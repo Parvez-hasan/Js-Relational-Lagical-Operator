57438f1a35e9d46535d7aef89d3c159f392961ed
Parvez-hasan <parvezp7788@gmail.com>
2025-06-19 22:42:37 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/* Relatoinal operator */
+// >, >=, <, <=, ==, ===, !=, !==
+
+/* var num1 = 25;
+var num2 = 30;
+
+console.log(num1>num2);
+console.log(25 < 30);
+console.log(20 <= 20);  
+
+console.log(20 == '20');
+console.log(20 === '20');  
+
+console.log(20 != 25);
+console.log( 25 != 25); */
+
+
+
+/* Logical operator */
+// &&, ||, |, ! 
+
+var num1 = 50;
+var num2 = 30;
+var num3 = 20; 
+
+ console.log(num1>num2 && num2>num3);
+ console.log(num1>num2 && num2<num3);
+
+ console.log(num1 > num2 || num3 > num2);
+ console.log(num3 > num1 || num2 > num1);
+ console.log(!true);
+ console.log(!false);
+
+console.log(!(25>22));
+console.log(!(25<22));
+
+
+
